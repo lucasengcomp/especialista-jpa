f842fd2806f7005fcf47d78bc2afb5dfa207f882
Lucas Galvao <lucas.engcomp@outlook.com>
2022-04-16 21:08:57 -0300

    Montando a classe para testes com o JUnit.

---
+++ backend/src/test/java/com/algaworks/ecommerce/iniciandocomjpa/IniciandoComJpa.java
@@ -0,0 +1,36 @@
+package com.algaworks.ecommerce.iniciandocomjpa;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class IniciandoComJpa {
+
+    private static EntityManagerFactory entityManagerFactory;
+
+    private EntityManager entityManager;
+
+    @BeforeClass
+    public static void setUpBeforeClass() {
+        entityManagerFactory = Persistence.createEntityManagerFactory("Ecommerce-PU");
+    }
+
+    @AfterClass
+    public static void tearDownAfterClass() {
+        entityManagerFactory.close();
+    }
+
+    @BeforeClass
+    public void setUp() {
+        entityManager = entityManagerFactory.createEntityManager();
+    }
+
+    @After
+    public void tearDown() {
+        entityManager.close();
+    }
+}
