e354ee78fc22bc1edc4703a0791c2c4bca481cb4
Lucas Galvao <lucas.engcomp@outlook.com>
2022-05-29 23:55:23 -0300

    Mapeando relacionamentos muitos-para-muitos com @ManyToMany e @JoinTable

---
+++ backend/src/test/java/com/algaworks/ecommerce/relacionamentos/RelacionamentoManyToManyTest.java
@@ -0,0 +1,27 @@
+package com.algaworks.ecommerce.relacionamentos;
+
+import com.algaworks.ecommerce.EntityManagerTest;
+import com.algaworks.ecommerce.model.Categoria;
+import com.algaworks.ecommerce.model.Produto;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class RelacionamentoManyToManyTest extends EntityManagerTest {
+
+    @Test
+    public void verificarRelacionamento() {
+        Produto produto = entityManager.find(Produto.class, 1);
+        Categoria categoria = entityManager.find(Categoria.class, 1);
+
+        entityManager.getTransaction().begin();
+        produto.setCategorias(Arrays.asList(categoria));
+        entityManager.getTransaction().commit();
+        entityManager.clear();
+
+        Categoria categoriaVerificacao = entityManager.find(Categoria.class, categoria.getId());
+        Assert.assertFalse(categoriaVerificacao.getProdutos().isEmpty());
+    }
+}
