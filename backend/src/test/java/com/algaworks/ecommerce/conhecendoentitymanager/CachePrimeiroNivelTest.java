689e1eb26a1c526d85088f9f107fa51ec3883b5e
Lucas Galvao <lucas.engcomp@outlook.com>
2022-06-05 13:55:46 -0300

    cache de primeiro nível

---
+++ backend/src/test/java/com/algaworks/ecommerce/conhecendoentitymanager/CachePrimeiroNivelTest.java
@@ -0,0 +1,19 @@
+package com.algaworks.ecommerce.conhecendoentitymanager;
+
+import com.algaworks.ecommerce.EntityManagerTest;
+import com.algaworks.ecommerce.model.Produto;
+import org.junit.Test;
+
+public class CachePrimeiroNivelTest extends EntityManagerTest {
+
+    @Test
+    public void verificarCache() {
+        Produto produto = entityManager.find(Produto.class, 1);
+
+        System.out.printf(produto.getNome());
+        System.out.printf("--------------------");
+
+        Produto produtoResgatado = entityManager.find(Produto.class, produto.getId());
+        System.out.println(produtoResgatado.getNome());
+    }
+}
--- backend/src/test/java/com/algaworks/ecommerce/mapeamentobasico/MapeamentoObjetoEmbutido.java
+++ backend/src/test/java/com/algaworks/ecommerce/mapeamentobasico/MapeamentoObjetoEmbutido.java
@@ -1,8 +1,10 @@
 package com.algaworks.ecommerce.mapeamentobasico;
 
 import com.algaworks.ecommerce.EntityManagerTest;
+import com.algaworks.ecommerce.model.Cliente;
 import com.algaworks.ecommerce.model.Pedido;
 import com.algaworks.ecommerce.model.embededs.EnderecoEntregaPedido;
+import com.algaworks.ecommerce.model.enums.StatusPedido;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -13,18 +15,23 @@ public class MapeamentoObjetoEmbutido extends EntityManagerTest {
 
     @Test
     public void analisarMapeamentoObjetoEmbutido() {
+        Cliente cliente = entityManager.find(Cliente.class, 1);
+
         EnderecoEntregaPedido endereco = new EnderecoEntregaPedido();
-        endereco.setCep("123456");
-        endereco.setLogradouro("Rua das laranjeiras");
+        endereco.setCep("00000-00");
+        endereco.setLogradouro("Rua das Laranjeiras");
+        endereco.setNumero("123");
         endereco.setBairro("Centro");
-        endereco.setNumero("123A");
-        endereco.setCidade("Anápolis");
-        endereco.setEstado("Goiás");
+        endereco.setCidade("Uberlândia");
+        endereco.setEstado("MG");
 
         Pedido pedido = new Pedido();
-//        pedido.setId(1);
+        // pedido.setId(1); Comentado porque estamos utilizando IDENTITY
         pedido.setDataPedido(LocalDateTime.now());
+        pedido.setStatus(StatusPedido.AGUARDANDO);
         pedido.setTotal(new BigDecimal(1000));
+        pedido.setEndereco(endereco);
+        pedido.setCliente(cliente);
 
         entityManager.getTransaction().begin();
         entityManager.persist(pedido);
@@ -33,7 +40,7 @@ public class MapeamentoObjetoEmbutido extends EntityManagerTest {
 
         Pedido pedidoVerificacao = entityManager.find(Pedido.class, pedido.getId());
         Assert.assertNotNull(pedidoVerificacao);
-        Assert.assertNotNull(pedidoVerificacao.getId());
+        Assert.assertNotNull(pedidoVerificacao.getEndereco());
         Assert.assertNotNull(pedidoVerificacao.getEndereco().getCep());
     }
 }
