72ba4cb2d2957118b39d19295756a48a37bd4c08
Lucas Galvao <lucas.engcomp@outlook.com>
2022-06-07 16:29:22 -0300

    Mapeando chave composta com @IdClass

--- backend/src/main/java/com/algaworks/ecommerce/model/ItemPedido.java
+++ backend/src/main/java/com/algaworks/ecommerce/model/ItemPedido.java
@@ -1,30 +1,36 @@
 package com.algaworks.ecommerce.model;
 
+import com.algaworks.ecommerce.model.chavecomposta.ItemPedidoId;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
-
 @Getter
 @Setter
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@IdClass(ItemPedidoId.class)
 @Entity
 @Table(name = "item_pedido")
 public class ItemPedido {
 
     @EqualsAndHashCode.Include
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    @Column(name = "pedido_id")
+    private Integer pedidoId;
+
+    @EqualsAndHashCode.Include
+    @Id
+    @Column(name = "produto_id")
+    private Integer produtoId;
 
     @ManyToOne(optional = false)
-    @JoinColumn(name = "pedido_id")
+    @JoinColumn(name = "pedido_id", insertable = false, updatable = false)
     private Pedido pedido;
 
     @ManyToOne(optional = false)
-    @JoinColumn(name = "produto_id")
+    @JoinColumn(name = "produto_id", insertable = false, updatable = false)
     private Produto produto;
 
     @Column(name = "preco_produto")
