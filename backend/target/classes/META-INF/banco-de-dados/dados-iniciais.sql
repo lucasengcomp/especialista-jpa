7175b40527e1c78e1636e25a4bead8fe2ce22527
Lucas Galvao <lucas.engcomp@outlook.com>
2022-06-07 14:59:05 -0300

    Adicionando insertable e updatable para Pedido

--- backend/target/classes/META-INF/banco-de-dados/dados-iniciais.sql
+++ backend/target/classes/META-INF/banco-de-dados/dados-iniciais.sql
@@ -1,5 +1,5 @@
-insert into produto (id, nome, preco, descricao) values (1, 'Kindle', 499.0, 'Conheça o novo Kindle, agora com iluminação embutida ajustável, que permite que você leia em ambientes abertos ou fechados, a qualquer hora do dia.');
-insert into produto (id, nome, preco, descricao) values (3, 'Câmera GO Pro', 1499.0, 'Uma boa camera para esportes.');
+insert into produto (id, nome, preco, data_criacao, descricao) values (1, 'Kindle', 499.0, date_sub(sysdate(), interval 1 day), 'Conheça o novo Kindle, agora com iluminação embutida ajustável, que permite que você leia em ambientes abertos ou fechados, a qualquer hora do dia.');
+insert into produto (id, nome, preco, data_criacao, descricao) values (3, 'Câmera GO Pro', 1499.0, date_sub(sysdate(), interval 1 day), 'Uma boa camera para esportes.');
 
 insert into cliente (id, nome) values (1, 'Ana Caroliny');
 insert into cliente (id, nome) values (5, 'Leila Galvao');
