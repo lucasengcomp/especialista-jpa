ed6c4df32656c4c92c72e6a9d1e9bb7e8f5ab2a6
Lucas Galvao <lucas.engcomp@outlook.com>
2022-07-11 15:43:30 -0300

    Selecionando parametro da query e todos os campos

--- backend/src/test/java/com/algaworks/ecommerce/jpql/BasicoJPQLTest.java
+++ backend/src/test/java/com/algaworks/ecommerce/jpql/BasicoJPQLTest.java
@@ -1,16 +1,29 @@
 package com.algaworks.ecommerce.jpql;
 
 import com.algaworks.ecommerce.EntityManagerTest;
+import com.algaworks.ecommerce.model.Cliente;
 import com.algaworks.ecommerce.model.Pedido;
 import org.junit.Assert;
 import org.junit.Test;
 
 import javax.persistence.Query;
 import javax.persistence.TypedQuery;
-import java.lang.reflect.Type;
 import java.util.List;
 
 public class BasicoJPQLTest extends EntityManagerTest {
+
+    @Test
+    public void selecionarUmAtributoParaRetorno() {
+        String jpql = "select p.nome from Produto p";
+        TypedQuery<String> typedQuery = entityManager.createQuery(jpql, String.class);
+        List<String> listaDeProdutos = typedQuery.getResultList();
+        Assert.assertTrue(String.class.equals(listaDeProdutos.get(0).getClass()));
+
+        String jpqlCliente = "select p.cliente from Pedido p";
+        TypedQuery<Cliente> typedQueryCliente = entityManager.createQuery(jpqlCliente, Cliente.class);
+        List<Cliente> listaDeClientes = typedQueryCliente.getResultList();
+        Assert.assertTrue(Cliente.class.equals(listaDeClientes.get(0).getClass()));
+    }
 
     @Test
     public void buscarPorIdentificador() {
