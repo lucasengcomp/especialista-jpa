885c45ed366103c1d02110ff428bb95c85e7e6f1
Lucas Galvao <lucas.engcomp@outlook.com>
2022-07-11 11:56:15 -0300

    Entendendo a remoção em cascata com @ManyToMany

--- backend/src/test/java/com/algaworks/ecommerce/operacoesemcascata/CascadeTypeRemoveTest.java
+++ backend/src/test/java/com/algaworks/ecommerce/operacoesemcascata/CascadeTypeRemoveTest.java
@@ -4,10 +4,25 @@ package com.algaworks.ecommerce.operacoesemcascata;
 import com.algaworks.ecommerce.EntityManagerTest;
 import com.algaworks.ecommerce.model.ItemPedido;
 import com.algaworks.ecommerce.model.Pedido;
+import com.algaworks.ecommerce.model.Produto;
 import com.algaworks.ecommerce.model.chavecomposta.ItemPedidoId;
 import org.junit.Assert;
 
 public class CascadeTypeRemoveTest extends EntityManagerTest {
+
+    //@Test
+    public void removerRelacaoProdutoCategoria() {
+        Produto produto = entityManager.find(Produto.class, 1);
+        Assert.assertFalse(produto.getCategorias().isEmpty());
+
+        entityManager.getTransaction().begin();
+        produto.getCategorias().clear();
+        entityManager.getTransaction().commit();
+        entityManager.clear();
+
+        Produto produtoVerificacao = entityManager.find(Produto.class, produto.getId());
+        Assert.assertTrue(produtoVerificacao.getCategorias().isEmpty());
+    }
 
     // @Test
     public void removerPedidoEItens() {
