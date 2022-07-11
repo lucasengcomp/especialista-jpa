c946101a4243ccf700e621f0ede90fc30acad779
Lucas Galvao <lucas.engcomp@outlook.com>
2022-07-11 15:09:44 -0300

    Buscando por identificador com JPQL

---
+++ backend/src/test/java/com/algaworks/ecommerce/jpql/BasicoJPQLTest.java
@@ -0,0 +1,22 @@
+package com.algaworks.ecommerce.jpql;
+
+import com.algaworks.ecommerce.EntityManagerTest;
+import com.algaworks.ecommerce.model.Pedido;
+import org.junit.Assert;
+import org.junit.Test;
+
+import javax.persistence.TypedQuery;
+
+public class BasicoJPQLTest extends EntityManagerTest {
+
+    @Test
+    public void buscarPorIdentificador() {
+        TypedQuery<Pedido> typedQuery = entityManager
+                .createQuery("select p from Pedido p where p.id = 1", Pedido.class);
+
+        Pedido pedido = typedQuery.getSingleResult();
+        Assert.assertNotNull(pedido);
+//        List<Pedido> resultList = typedQuery.getResultList();
+//        Assert.assertFalse(resultList.isEmpty());
+    }
+}
