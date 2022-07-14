ca8f53e4f6404ec5bb2263635763d06736f68123
Lucas Galvao <lucas.engcomp@outlook.com>
2022-07-14 15:39:31 -0300

    Exercicio comparação de datas

--- backend/src/test/java/com/algaworks/ecommerce/jpql/ExpressoesCondicionaisTest.java
+++ backend/src/test/java/com/algaworks/ecommerce/jpql/ExpressoesCondicionaisTest.java
@@ -1,14 +1,28 @@
 package com.algaworks.ecommerce.jpql;
 
 import com.algaworks.ecommerce.EntityManagerTest;
+import com.algaworks.ecommerce.model.Pedido;
 import org.junit.Assert;
 import org.junit.Test;
 
 import javax.persistence.TypedQuery;
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.List;
 
 public class ExpressoesCondicionaisTest extends EntityManagerTest {
+
+    @Test
+    public void usarComparacaoDeDatas() {
+        String jpql = "SELECT p FROM Pedido p " +
+                "WHERE p.dataCriacao < :data";
+
+        TypedQuery<Pedido> typedQuery = entityManager.createQuery(jpql, Pedido.class);
+        typedQuery.setParameter("data", LocalDateTime.now().minusDays(2));
+        List<Pedido> lista = typedQuery.getResultList();
+
+        Assert.assertFalse(lista.isEmpty());
+    }
 
     @Test
     public void usarMaiorMenor() {
