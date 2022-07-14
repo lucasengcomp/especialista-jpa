3acfcae7dc16353537b1c0a0606bb95f5f44d5a7
Lucas Galvao <lucas.engcomp@outlook.com>
2022-07-14 16:50:46 -0300

    Ordenação de resultados com ORDER BY

--- backend/src/test/java/com/algaworks/ecommerce/jpql/BasicoJPQLTest.java
+++ backend/src/test/java/com/algaworks/ecommerce/jpql/BasicoJPQLTest.java
@@ -14,8 +14,20 @@ import java.util.List;
 public class BasicoJPQLTest extends EntityManagerTest {
 
     @Test
+    public void ordenarResultado() {
+        String jpql = "SELECT c FROM Cliente c " +
+                "ORDER BY c.nome";
+
+        TypedQuery<Cliente> typedQuery = entityManager.createQuery(jpql, Cliente.class);
+        List<Cliente> lista = typedQuery.getResultList();
+
+        Assert.assertFalse(lista.isEmpty());
+        lista.forEach(p -> System.out.println(p.getId() + ", " + p.getNome()));
+    }
+
+    @Test
     public void projetoNoDTO() {
-        String jpql = "select new com.algaworks.ecommerce.dto.ProdutoDTO(id, nome) from Produto";
+        String jpql = "SELECT new com.algaworks.ecommerce.dto.ProdutoDTO(id, nome) FROM Produto";
 
         TypedQuery<ProdutoDTO> typedQuery = entityManager.createQuery(jpql, ProdutoDTO.class);
         List<ProdutoDTO> lista = typedQuery.getResultList();
@@ -26,7 +38,7 @@ public class BasicoJPQLTest extends EntityManagerTest {
 
     @Test
     public void projetarOResultado() {
-        String jpql = "select id, nome from Produto";
+        String jpql = "SELECT id, nome FROM Produto";
 
         TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
         List<Object[]> lista = typedQuery.getResultList();
@@ -37,31 +49,31 @@ public class BasicoJPQLTest extends EntityManagerTest {
 
     @Test
     public void selecionarUmAtributoParaRetorno() {
-        String jpql = "select p.nome from Produto p";
+        String jpql = "SELECT p.nome FROM Produto p";
+
         TypedQuery<String> typedQuery = entityManager.createQuery(jpql, String.class);
         List<String> listaDeProdutos = typedQuery.getResultList();
         Assert.assertTrue(String.class.equals(listaDeProdutos.get(0).getClass()));
 
-        String jpqlCliente = "select p.cliente from Pedido p";
+        String jpqlCliente = "SELECT p.cliente FROM Pedido p";
         TypedQuery<Cliente> typedQueryCliente = entityManager.createQuery(jpqlCliente, Cliente.class);
         List<Cliente> listaDeClientes = typedQueryCliente.getResultList();
+
         Assert.assertTrue(Cliente.class.equals(listaDeClientes.get(0).getClass()));
     }
 
     @Test
     public void buscarPorIdentificador() {
         TypedQuery<Pedido> typedQuery = entityManager
-                .createQuery("select p from Pedido p where p.id = 1", Pedido.class);
+                .createQuery("SELECT p FROM Pedido p WHERE p.id = 1", Pedido.class);
 
         Pedido pedido = typedQuery.getSingleResult();
         Assert.assertNotNull(pedido);
-//        List<Pedido> resultList = typedQuery.getResultList();
-//        Assert.assertFalse(resultList.isEmpty());
     }
 
     @Test
     public void mostrarDiferencaQueries() {
-        String jpql = "select p from Pedido p where p.id = 1";
+        String jpql = "SELECT p FROM Pedido p WHERE p.id = 1";
         TypedQuery<Pedido> typedQuery = entityManager.createQuery(jpql, Pedido.class);
 
         Pedido pedido1 = typedQuery.getSingleResult();
@@ -69,9 +81,7 @@ public class BasicoJPQLTest extends EntityManagerTest {
 
         Query query = entityManager.createQuery(jpql);
         Pedido pedido2 = (Pedido) query.getSingleResult();
-        Assert.assertNotNull(pedido2);
 
-//        List<Pedido> listaDePedidos = query.getResultList();
-//        Assert.assertFalse(listaDePedidos.isEmpty());
+        Assert.assertNotNull(pedido2);
     }
 }
