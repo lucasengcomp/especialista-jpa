4a288868ff10cbe915b68cf4c59258fd2aa3cf75
Lucas Galvao <lucas.engcomp@outlook.com>
2022-07-14 15:17:06 -0300

    Testando is empty de Produto

--- backend/src/test/java/com/algaworks/ecommerce/jpql/ExpressoesCondicionaisTest.java
+++ backend/src/test/java/com/algaworks/ecommerce/jpql/ExpressoesCondicionaisTest.java
@@ -1,7 +1,6 @@
 package com.algaworks.ecommerce.jpql;
 
 import com.algaworks.ecommerce.EntityManagerTest;
-import com.algaworks.ecommerce.model.Cliente;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -9,6 +8,28 @@ import javax.persistence.TypedQuery;
 import java.util.List;
 
 public class ExpressoesCondicionaisTest extends EntityManagerTest {
+
+    @Test
+    public void usarIsEmpty() {
+        String jpql = "SELECT p FROM Produto p " +
+                "WHERE p.categorias is empty";
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+        List<Object[]> lista = typedQuery.getResultList();
+
+        Assert.assertFalse(lista.isEmpty());
+    }
+
+    @Test
+    public void usarIsNull() {
+        String jpql = "SELECT p FROM Produto p " +
+                "WHERE p.foto is null";
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+        List<Object[]> lista = typedQuery.getResultList();
+
+        Assert.assertFalse(lista.isEmpty());
+    }
 
     @Test
     public void usarExpressaoCondicionalLike() {
